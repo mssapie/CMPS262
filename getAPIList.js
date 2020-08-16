5da118b85792a1102795bf6a96f382b211df85e1
Michael Sapienza <mssapie@pointpark.edu>
2020-08-15 21:23:55 -0400

    Adding more Java protocol

---
+++ getAPIList.js
@@ -0,0 +1,31 @@
+var url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=covid-19&api-key=ocmYAnlVxm3QWnQoHJtBwtUdiu90MlAn'
+
+function setup() {
+  noCanvas();
+  loadJSON(url, getData);
+}
+function getData(data) {
+  var articles = data.response.docs;
+
+  //Loop to get all results from articlesearch
+  for (var i = 0; i < articles.length; i++) {
+    createElement('h1', articles[i].headline.main);
+    createP(articles[i].snippet);
+    createP(articles[i].lead_paragraph);
+    createP(articles[i].web_url);
+  }
+}
+
+
+function setup() {
+  noCanvas();
+  loadJSON(url, getData);
+}
+function getData(data) {
+  var articles = data.response.docs;
+
+  //Loop to get all results from articlesearch
+  for (var i = 0; i < articles.length; i++){
+    createElement('h1', articles[i].headline.main);
+  }
+}
